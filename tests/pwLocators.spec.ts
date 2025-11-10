49e9d97656d8d36617f17b7fde4a64d51ca585f7
Prasanth-P29 <prasanth477361@gmail.com>
2025-11-10 22:59:42 +0530

    concepts

--- tests/pwLocators.spec.ts
+++ tests/pwLocators.spec.ts
@@ -1,5 +1,18 @@
-//PlayWright Locators
-//DOM is an API Interface provided by Browsers
+                                            // PlayWright Locators
+
+// DOM is an API Interface provided by Browsers
+// Locator - Identifies the element on the page.
+// DOM - Document Object Model
+// DOM is an API Interface provided by browser.
+
+// 1) page.getByAltText() to locate an element, usually image, by its text alternative.
+// 2) page.getByText() to locate by text content.(Non interactive elements)
+// 3) page.getByRole() to locate by explicit and implicit accessibility attributes.
+// 4) page.getByLabel() to locate a form control by associated label's text.
+// 5) page.getByPlaceholder() to locate an input by placeholder.
+
+// 6) page.getByTitle() to locate an element by its title attribute.
+// 7) page.getByTestId() to locate an element based on its data-testid attribute (other attributes can be configure
 
 import { test, expect, Locator } from "@playwright/test";
 
@@ -48,8 +61,16 @@ test("Verify Page Logo", async ({page}) => {
     await page.getByLabel('First name').fill("Prasanth");
     await page.getByLabel('Last name').fill("Kumar");
     await page.getByLabel('Email').fill("prasanth.kumar@example.com");
-    
 
+
+    // 5. page.getByPlaceholder() - find element with a given placeholder text
+    // Best for inputs without a label but having a placeholder 
+    await page.getByPlaceholder('Search store').fill("Iphone 17 pro max");
+
+
+    // 6. page.getByTitle() to locate an element by its title attribute.
+    // When to use: When your element has a meaningful title attribute.
+    await page.goto("");
 
 
 });
